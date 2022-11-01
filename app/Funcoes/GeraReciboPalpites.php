90a6ccb224530379ef206af445d409198eebecf2
Cleiton Rieke <cleitonrieke@gmail.com>
2022-10-31 21:04:37 -0300

    teste imagem

--- app/Funcoes/GeraReciboPalpites.php
+++ app/Funcoes/GeraReciboPalpites.php
@@ -27,18 +27,6 @@ class GeraReciboPalpites
 
         $this->ds_arquivo = $this::DIRETORIO_TMP.'/'.date('Y-m-d').'_'.date('H:i:s').'_recibo_palpites.pdf';
         
-        //############ Permitir ver imagenes si falla ################################
-          $contxt = stream_context_create([
-            'ssl' => [
-                'verify_peer' => FALSE,
-                'verify_peer_name' => FALSE,
-                'allow_self_signed' => TRUE,
-            ]
-        ]);
-
-        $pdf = \PDF::setOptions(['isHTML5ParserEnabled' => true, 'isRemoteEnabled' => true]);
-        $pdf->getDomPDF()->setHttpContext($contxt);
-        //#################################################################################
-        $this->pdf = PDF::loadView('domPdf.reciboPalpites');
+        $this->pdf = PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true ,'chroot' => public_path()])->setPaper('a4', 'portrait')->loadView('domPdf.reciboPalpites');
     }
 }
